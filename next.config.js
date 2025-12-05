597cfc472c4fe08c0a4f4a594e6c9af47c19d4dc
sungbin <dusvlf5950@naver.com>
2025-12-06 02:43:17 +0900

    Enable standalone output in Next.js config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'standalone',
 }
 
 module.exports = withPWA(nextConfig)
